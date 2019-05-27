c31aa1898298824b72e3d37fed16378d4a0fcdf7
DebianLynx <48427119+DebianLynx@users.noreply.github.com>
2019-05-27 13:10:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,12 @@ git clone https://github.com/DebianLynx/termuxsh.git
 cd termuxsh
 chmod +x service 
 mv ./service /data/data/com.termux/files/usr/bin/
-mkdir /data/data/com.termux/files/usr/etc/service
+
+if [ -f /data/data/com.termux/files/usr/etc/service ]; then
+    echo "service dir areadly created,maybe it's a update install"
+else
+    mkdir /data/data/com.termux/files/usr/etc/service
+fi 
+
+
 
