e8a8a684e6e4929f985bfa3cd4d06c774a2fb909
DebianLynx <48427119+DebianLynx@users.noreply.github.com>
2019-08-27 17:25:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,42 @@
-apt update
-apt install git
 
+install_tools() {
+apt update
+apt install wget unzip
+#install tools
+}
+
+
+download() {
+cd ~
 if [ -d ./termuxsh ]; then
     rm -rf ./termuxsh 
 fi 
+wget https://fuckchina.ga/termuxsh.zip
+unzip ./termux.zip
+cd ./termuxsh
+}
 
-git clone https://github.com/DebianLynx/termuxsh.git
-cd termuxsh
+
+installservice() {
+cd ~/termuxsh
 chmod +x service 
 mv ./service /data/data/com.termux/files/usr/bin/
 
+#mkdir
 if [ -d /data/data/com.termux/files/usr/etc/service ]; then
     echo "service dir areadly created,maybe it's an update install"
 else
     mkdir /data/data/com.termux/files/usr/etc/service
-fi 
+fi
+}
 
 
-
+installconf() {
+cd ~/termuxsh
+chmod +x conf
+if [ -d /data/data/com.termux/files/usr/etc/service ]; then
+    echo "conf dir areadly created,maybe it's an update install"
+else
+    mkdir /data/data/com.termux/files/usr/etc/conf
+fi
+}
